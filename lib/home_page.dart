e56a661aff12b84659241b9cdf8814a5af2abd4b
imaNNeoFighT <iman.neofight@gmail.com>
2020-09-21 23:29:59 +0330

    Finished curved_line_widget design

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+
+import 'widgets/curved_line_widget.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color(0x060524),
       body: Center(
         child: Center(
           child: Padding(
@@ -15,46 +18,4 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
-}
-
-class CurvedLineWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return CustomPaint(
-      painter: _CurvedLineCustomPainter(20),
-      size: Size(double.infinity, 100),
-    );
-  }
-
-}
-
-
-class _CurvedLineCustomPainter extends CustomPainter {
-
-  final double curveHeight;
-
-  Paint linePaint = Paint()
-    ..color = Colors.lightBlueAccent
-    ..style = PaintingStyle.stroke..strokeWidth = 8
-    ..strokeCap = StrokeCap.round;
-
-  _CurvedLineCustomPainter(this.curveHeight);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-
-    final point1 = Offset(0, size.height / 2);
-    final point2 = Offset(size.width , size.height / 2);
-    final controlPoint = Offset((point2.dx - point1.dx) / 2, (size.height / 2) - (curveHeight * 2));
-
-    Path path = new Path();
-    path.moveTo(point1.dx, point1.dy);
-    path.quadraticBezierTo(controlPoint.dx, controlPoint.dy, point2.dx, point2.dy);
-
-    canvas.drawPath(path, linePaint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) => true;
-
 }
--- lib/widgets/curved_line_widget.dart
+++ lib/widgets/curved_line_widget.dart
@@ -1,59 +1,84 @@
+import 'dart:ui' as ui;
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class CurvedLineWidget extends StatefulWidget {
+class CurvedLineWidget extends StatelessWidget {
+  final double curveHeight, lineThickness, gradientHeight;
+  final Color lineColor;
 
-  @override
-  _CurvedLineWidgetState createState() => _CurvedLineWidgetState();
-}
-
-class _CurvedLineWidgetState extends State<CurvedLineWidget> {
-  Offset touchedPoint;
+  const CurvedLineWidget({
+    Key key,
+    this.curveHeight = 20,
+    this.lineThickness = 8,
+    this.gradientHeight = 68,
+    this.lineColor = Colors.lightBlueAccent,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      child: CustomPaint(
-        painter: _CurvedLineCustomPainter(touchedPoint),
-        size: Size(double.infinity, 100),
+    return CustomPaint(
+      painter: _CurvedLineCustomPainter(
+        curveHeight,
+        lineThickness,
+        gradientHeight,
+        lineColor,
       ),
-      onPanUpdate: (DragUpdateDetails details) {
-        setState(() {
-          touchedPoint = details.localPosition;
-        });
-      },
+      size: Size(double.infinity, gradientHeight),
     );
   }
 }
 
 class _CurvedLineCustomPainter extends CustomPainter {
+  final double curveHeight;
+  final double lineThickness;
+  final double gradientHeight;
+  final Color lineColor;
 
-  final Offset touchedPoint;
+  Paint linePaint, glowAreaPaint;
 
-  Paint linePaint = Paint()..color = Colors.lightBlueAccent..style = PaintingStyle.stroke..strokeWidth = 8;
+  _CurvedLineCustomPainter(
+    this.curveHeight,
+    this.lineThickness,
+    this.gradientHeight,
+    this.lineColor,
+  ) {
+    linePaint = Paint()
+      ..color = lineColor
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = lineThickness
+      ..strokeCap = StrokeCap.round;
 
-  _CurvedLineCustomPainter(this.touchedPoint);
+    glowAreaPaint = Paint();
+  }
 
   @override
   void paint(Canvas canvas, Size size) {
+    final point1 = Offset((lineThickness / 2), curveHeight);
+    final point2 = Offset(size.width - (lineThickness / 2), curveHeight);
+    final controlPoint =
+        Offset((point2.dx - point1.dx) / 2, point1.dy - (curveHeight * 2) + lineThickness);
 
-    final point1 = Offset(0, 80);
-    final point2 = Offset(size.width , 80);
-    final controlPoint = touchedPoint;
+    Path linePath = new Path();
+    linePath.moveTo(point1.dx, point1.dy);
+    linePath.quadraticBezierTo(controlPoint.dx, controlPoint.dy, point2.dx, point2.dy);
 
-    Path path = new Path();
-    path.moveTo(point1.dx, point1.dy);
-    path.quadraticBezierTo(controlPoint.dx, controlPoint.dy, point2.dx, point2.dy);
+    Path glowAreaPath = Path.from(linePath);
+    glowAreaPath.lineTo(point2.dx, point2.dy + gradientHeight);
+    glowAreaPath.lineTo(point1.dx, point1.dy + gradientHeight);
+    glowAreaPath.lineTo(point1.dx, point1.dy); // step3
 
-    canvas.drawPath(path, linePaint);
+    glowAreaPaint
+      ..shader = ui.Gradient.linear(
+        Offset(size.width / 2, 0),
+        Offset(size.width / 2, gradientHeight),
+        [Colors.blueAccent.withOpacity(0.4), Colors.blueAccent.withOpacity(0)],
+      );
 
-    canvas.drawCircle(point1, 8, Paint()..color = Colors.purple);
-    canvas.drawCircle(point2, 8, Paint()..color = Colors.green);
-    canvas.drawCircle(controlPoint, 8, Paint()..color = Colors.red);
+    canvas.drawPath(glowAreaPath, glowAreaPaint);
+    canvas.drawPath(linePath, linePaint);
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => true;
-
 }
