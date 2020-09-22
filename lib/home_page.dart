28c6cb6522523127d61f60da3e6b4528492f2bac
imaNNeoFighT <iman.neofight@gmail.com>
2020-09-22 22:38:44 +0330

    Combined main widgets and added in home

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ui_challenge_1/widgets/curved_line_widget.dart';
 import 'package:ui_challenge_1/widgets/seats_grid.dart';
 import 'colors.dart';
 import 'widgets/header_section.dart';
@@ -19,7 +20,37 @@ class HomePage extends StatelessWidget {
                 padding: const EdgeInsets.symmetric(horizontal: 16.0),
                 child: HeaderSection(),
               ),
+              SizedBox(
+                height: 32,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10.0,
+                ),
+                child: CurvedLineWidget(
+                  lineThickness: 6,
+                  lineColor: sceneColor1,
+                  gradientHeight: 42,
+                  gradientColors: [sceneColor1.withOpacity(0.35), sceneColor1.withOpacity(0)],
+                ),
+              ),
+              SizedBox(
+                height: 14,
+              ),
               SeatsGrid(),
+              SizedBox(
+                height: 8,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(
+                  horizontal: 16,
+                ),
+                height: 0.7,
+                color: Color(0xffA2A2A2).withOpacity(0.3),
+              ),
+              SizedBox(
+                height: 8,
+              ),
             ],
           ),
         ),
